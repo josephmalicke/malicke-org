05c1710140ad61fc61b1b006cff5db93a9df62ba
Joseph Malicke <joseph@malicke.org>
2025-10-01 21:38:11 +0000

    Add tailwind allowed dev origin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    'honeycomb',
+  ],
 };
 
 export default nextConfig;
